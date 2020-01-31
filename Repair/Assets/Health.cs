2256e5775abb1f77fedf098d57a0be95ee089914
Tugay Mandal <tugay.mandal@gmail.com>
2020-01-31 20:57:06 +0300

    Movement and Health Added

---
+++ Repair/Assets/Health.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 5;
+
+    int currentHealth;
+
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+    }
+
+
+
+}
---
+++ Repair/Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 3f;
+
+
+
+    Rigidbody rb;
+
+
+    float horizontal;
+    bool isJumping;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+
+        horizontal = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontal,0) * speed * Time.deltaTime;
+
+        if (Input.GetAxis("Jump") != 0 && isJumping == false)
+        {
+            rb.AddForce(new Vector3(0, jumpForce) * Time.deltaTime * 5000f);
+            isJumping = true;
+        }
+
+
+
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isJumping = false;
+        }
+    }
+}
