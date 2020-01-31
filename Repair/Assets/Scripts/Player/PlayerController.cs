99ca4a1f980323a86fac733710099cc88d7576b8
Kizi Tekuto <paradoxht@hotmail.com>
2020-01-31 21:53:23 +0300

    fixed player controller

--- Repair/Assets/Scripts/Player/PlayerController.cs
+++ Repair/Assets/Scripts/Player/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Health), typeof(UIController))]
-class PlayerController
+class PlayerController : MonoBehaviour
 {
 
 }
