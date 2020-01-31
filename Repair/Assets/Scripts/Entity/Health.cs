2e9248025ab1d8d2fe67bbd1fd729b64f7baaea5
Kizi Tekuto <paradoxht@hotmail.com>
2020-01-31 21:12:46 +0300

    clean up, health update methods, player controller added

---
+++ Repair/Assets/Scripts/Entity/Health.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] uint maxHealth = 5;
+
+    private uint currentHealth;
+    public uint CurrentHealth
+    {
+        get
+        {
+            return currentHealth;
+        }
+        set
+        {
+            currentHealth = (uint)Mathf.Clamp(value, 0, maxHealth);
+
+            (currentHealth == 0 ? Died : HealthUpdated)?.Invoke();
+        }
+    }
+    public static Action HealthUpdated;
+    public static Action Died;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+}
---
+++ Repair/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health), typeof(UIController))]
+class PlayerController
+{
+
+}
+
